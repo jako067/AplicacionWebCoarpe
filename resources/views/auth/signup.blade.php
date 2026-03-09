071ad09b253ad38e1fb02850c8adc1d0289f85a8
Diego <riveradiegoyancha@gmail.com>
2026-03-09 21:20:01 +0100

    Commit Diego

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- It is never too late to be what you might have been. - George Eliot -->
-</div>
+@section('title','Signup')
+@section('body')
+
+    Aquí será el Signup
+
+    <form action="{{route('signup')}}" method="post">
+        @csrf
+
+        <label for="username"> Nombre de Usuario:</label> <br>
+        <input type="text" name="username" id="username" value="{{old('username')}}"><br>
+        <label for="name"> Nombre de Usuario:</label> <br>
+        <input type="text" name="name" id="name" value="{{old('name')}}"><br>
+        <label for="email"> Nombre de Usuario:</label> <br>
+        <input type="text" name="email" id="email" value="{{old('email')}}"><br>
+        <label for="password"> Nombre de Usuario:</label> <br>
+        <input type="text" name="password" id="password" value="{{old('password')}}"><br>
+
+    </form>
+
+@endsection
+
