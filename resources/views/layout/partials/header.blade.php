74037a2e565b079cb1af7fb81f25b83b583067ef
Diego <riveradiegoyancha@gmail.com>
2026-04-19 09:35:01 +0200

    merged

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -12,4 +12,6 @@
 <a href="{{ route('materials.index') }}">Materials</a>
 <a href="{{ route('budgets.index') }}">Budgets</a>
 <a href="{{ route('users.index') }}">Usuarios</a>
+<a href="{{ route('tasks.index') }}">Tareas</a>
+<a href="{{ route('messages.index') }}">Mensajes</a>
 
